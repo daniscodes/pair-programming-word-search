4ec4598ae9486e0d7417cded51033336df2ce424
daniscodes <dan97@bell.net>
2022-08-04 22:00:38 -0400

    Finished functionality

--- wordsearch.js
+++ wordsearch.js
@@ -1,19 +1,37 @@
+const arrange = function (matrix) {
+
+  var newArray = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+      newArray[i] = [];
+      counter = 0;
+      while (counter !== matrix.length) {
+          newArray[i].push(matrix[counter][i]);
+          counter++;
+      }
+  }
+  return newArray;
+};
+
+const findWord = function(strings, word) {
+  for (let letter of strings) {
+      if (letter.includes(word)) {
+          return true;
+      }
+  }
+};
+
 const wordSearch = (letters, word) => {
   if (letters.length < 1 || word.length < 1) return false;
-  const horizontalJoin = letters.map((ls) => ls.join(""));
-  const verticalJoin = letters[0].map((col, index) =>
-    letters.map((row) => row[index])
-  );
-  const verticalFlip = verticalJoin.map((ls) => ls.join(""));
-  for (l of horizontalJoin) {
-    if (l.includes(word)) return true;
-    else {
-      for (l of verticalFlip) {
-        if (l.includes(word)) return true;
-      }
-    }
+  
+  const verticalJoin = arrange(letters).map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const reverseJoin = letters.map(ls=>ls.reverse().join(''));
+  
+  if (findWord(verticalJoin, word) || findWord(horizontalJoin, word) || findWord(reverseJoin, word)) {
+      return true;
+  } else {
+      return false;
   }
-  return false;
 };
 
 module.exports = wordSearch;
